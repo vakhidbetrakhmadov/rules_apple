c23727a542618e3870d08dcf7ba021ffa470e05f
Vakhid Betrakhmadov <restermans.gm@gmail.com>
2024-11-27 21:45:32 +0000

    core data repro

---
+++ examples/repro/FooApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+public struct FooApp: App {
+  public init() { }
+
+  public var body: some Scene {
+    WindowGroup {
+      Text("Hello World")
+    }
+  }
+}
---
+++ examples/repro/FooTests.swift
@@ -0,0 +1,1 @@
+// FooTests.swift
